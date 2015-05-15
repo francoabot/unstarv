ead5a4d612f18d359376b63e11510926988efda2
Moussa Sarr <moussasarr490@gmail.com>
2015-05-14 23:30:47 -0700

    Still the FB signin

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 OmniAuth.config.logger = Rails.logger
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['1427074967594599'], ENV['14591fbffa0f7b25606f0e6102635ac2']
+  provider :facebook, ENV["1427074967594599"], ENV["14591fbffa0f7b25606f0e6102635ac2"]
 end
