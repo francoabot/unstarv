dfa9319db185380ca17e9494bdc2d9ac027c23e0
Moussa Sarr <moussasarr490@gmail.com>
2015-05-16 19:12:48 -0700

    Add password resets & email configuration

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'omniauth-twitter'
 gem 'omniauth-facebook'
 gem 'omniauth-linkedin'
 gem 'omniauth'
+gem 'omniauth-identity'
 
 gem 'activerecord-reputation-system', require: 'reputation_system'
 # Use sqlite3 as the database for Active Record
@@ -51,6 +52,7 @@ end
 group :production do
   gem 'pg',             '0.17.1'
   gem 'rails_12factor', '0.0.2'
+  gem 'activerecord-postgresql-adapter'
 end
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,19 +48,19 @@ Rails.application.configure do
   config.app_domain = 'unstarv.herokuapp.com'
 
   # Email
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: config.app_domain }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', 
-    port: '587',
-    enable_starttls_auto: true,
-    user_name: 'someuser',
-    password: 'somepass',
-    authentication: :plain,
-    domain: 'unstarv.herokuapp.com'
+  host = 'unstarv.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
-
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
