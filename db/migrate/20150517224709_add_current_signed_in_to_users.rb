f7032fbbd847d8a847d50d45ca3536c1db2b1015
Moussa Sarr <moussasarr490@gmail.com>
2015-05-19 14:07:43 -0700

    solved current_sign_at

--- db/migrate/20150517224709_add_current_signed_in_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddCurrentSignedInToUsers < ActiveRecord::Migration
-  def change
-  	
-  	add_column :users, :current_sign_in_at, :datetime
-  end
-end
--- db/migrate/20150517230150_solve.rb
+++ db/migrate/20150517230150_solve.rb
@@ -1,6 +1,6 @@
 class Solve < ActiveRecord::Migration
   def change
-  	
+  	drop_column :users, :current_sign_in_at
   	add_column :users, :current_sign_in_at, :datetime
   end
 end
