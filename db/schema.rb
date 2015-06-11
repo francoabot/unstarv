34a71512c92f9aa42c8930e02db6b2d506d29b0a
Moussa Sarr <moussasarr490@gmail.com>
2015-06-11 14:39:46 -0700

    Trying to resolve heroku prblem/twitter

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610234834) do
+ActiveRecord::Schema.define(version: 20150611212708) do
 
   create_table "comments", force: true do |t|
     t.string   "thor_name"
@@ -178,6 +178,7 @@ ActiveRecord::Schema.define(version: 20150610234834) do
     t.integer  "score",                default: 0
     t.boolean  "active",               default: true
     t.string   "artist",               default: "No"
+    t.string   "reset_password_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
