b432da6d6c6aa4e0ac42c2b2d31c926b23bd258e
Moussa Sarr <moussasarr490@gmail.com>
2015-06-12 00:53:15 -0700

    Blabla

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -21,7 +21,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     }
   end
 
-  [:twitter, :facebook,:google_oauth2, :linked_in ].each do |provider|
+  [:twitter, :facebook,:google_oauth2, :linkedin ].each do |provider|
     provides_callback_for provider
   end
   
