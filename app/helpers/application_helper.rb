78ced1bbe38d76118f720545cd9c593b73e26fc0
Moussa Sarr <moussasarr490@gmail.com>
2015-06-16 00:31:04 -0700

    Added gravatar for group

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,8 +20,12 @@ module ApplicationHelper
     content_for(:page_header) { text.to_s }
   end
 
-  def gravatar_for(user, size = 90, title = user.name)
+  def gravatar_for(user, size = 90, title = user.name )
     image_tag gravatar_image_url(user.email, size: size), title: title, class: 'img-rounded'
+  end
+
+  def gravatar_for_group(group, size = 90, title = group.group_name )
+    image_tag gravatar_image_url(group.group_name, size: size), title: title, class: 'img-rounded'
   end
 
 end
